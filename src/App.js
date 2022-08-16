18fa571e503ebdf1a76e35dc6c737e76dcbe9ded
Beka Sturua <beqasturua3@gmail.com>
2022-08-16 23:25:38 +0400

    add Navigate code in comment (i have error)

--- src/App.js
+++ src/App.js
@@ -18,3 +18,42 @@ function App() {
 }
 
 export default App;
+
+// Error Navigate ?????
+
+// import { useContext } from "react";
+// import { Routes, Route, Navigate } from "react-router-dom";
+
+// import Layout from "./components/Layout/Layout";
+// import UserProfile from "./components/Profile/UserProfile";
+// import AuthPage from "./pages/AuthPage";
+// import HomePage from "./pages/HomePage";
+// import AuthContext from "./store/auth-context";
+
+// function App() {
+//   const authCtx = useContext(AuthContext);
+
+//   return (
+//     <Layout>
+//       <Routes>
+//         <Route path="/" exact>
+//           <HomePage />
+//         </Route>
+//         {!authCtx.isLoggedIn && (
+//           <Route path="/auth">
+//             <AuthPage />
+//           </Route>
+//         )}
+//         <Route path="/profile">
+//           {authCtx.isLoggedIn && <UserProfile />}
+//           {!authCtx.isLoggedIn && <Navigate to="/auth" />}
+//         </Route>
+//         <Route path="*">
+//           <Navigate to="/" />
+//         </Route>
+//       </Routes>
+//     </Layout>
+//   );
+// }
+
+// export default App;
