33234ba309698d22427b323ba615a6cdf79a2015
Beka Sturua <beqasturua3@gmail.com>
2022-08-16 15:07:12 +0400

    feat: Add User Login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,38 +19,50 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     // optopmal: Add validation
+
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCFbTBOdPCc9AVA55ZtJpzjBv66m6YkxGE";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFbTBOdPCc9AVA55ZtJpzjBv66m6YkxGE",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-          // ...
-        } else {
-          return res.json().then((data) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFbTBOdPCc9AVA55ZtJpzjBv66m6YkxGE";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res
+          .json()
+          .then((data) => {
             let errorMessage = "Authentication failed!";
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((err) => {
+            alert(err.message);
           });
-        }
-      });
-    }
+      }
+    });
   };
 
   return (
