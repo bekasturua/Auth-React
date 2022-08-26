575983a9a5634c7a5f25eed9a716de0709ad97b9
Beka Sturua <beqasturua3@gmail.com>
2022-08-26 16:42:50 +0400

    feat: Add Auto-Logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,19 +7,33 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expitationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpitationTime = new Date(expitationTime).getTime();
+
+  const remaingDuration = adjExpitationTime - currentTime;
+
+  return remaingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expitationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expitationTime);
+
+    setTimeout(logoutHandler, 3000);
   };
 
   const contextValue = {
